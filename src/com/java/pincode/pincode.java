a425d56c5bdc9d7e0557d5d1090e30a430338350
swapnaneel bhuiya <bhuiya.swapnaneel@gmail.com>
2020-09-28 11:38:02 +0530

    UC4:Check the presence of spaces in between

--- src/com/java/pincode/pincode.java
+++ src/com/java/pincode/pincode.java
@@ -6,8 +6,6 @@ public class pincode {
 		Scanner sc=new Scanner(System.in);
 		System.out.println("Enter Pin Code");
 		String code=sc.nextLine();
-		if(code.length()!=6)
-			{System.out.println("Invalid Pin Code");System.exit(0);}
 		if(code.matches("^[1-9]{1}[0-9]{2}\\s{0,1}[0-9]{3}$"))
 			System.out.println("Valid PinCode");
 		else
